7faa0f3de72507a2d65fde156b8e176617065090
AlessandroDiNardo <alexdna003@gmail.com>
2023-02-06 18:56:53 +0100

    create table and add it in db

--- database/migrations/2023_02_06_173157_create_people_table.php
+++ database/migrations/2023_02_06_173157_create_people_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('people', function (Blueprint $table) {
+
             $table->id();
+            $table->string("firstName");
+            $table->string("lastName");
+            $table->date("dateOfBirth");
+            $table->integer("height")->unsigned();
             $table->timestamps();
         });
     }
