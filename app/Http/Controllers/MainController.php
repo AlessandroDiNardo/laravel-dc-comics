92f61657658c9a13f1f9f831128d6af254df8c04
AlessandroDiNardo <alexdna003@gmail.com>
2023-02-06 19:03:23 +0100

    create files in view

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,8 +3,82 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Person;
 
 class MainController extends Controller
 {
-    //
+    // home
+    public function home()
+    {
+        $persons = Person::all();
+
+        return view("pages.home", compact("persons"));
+    }
+
+    // show
+    public function showPerson(Person $person)
+    {
+        return view("pages.show-person", compact("person"));
+    }
+
+    // delete
+    public function deletePerson(Person $person)
+    {
+        $person->delete();
+        return redirect()->route("home");
+    }
+
+    // create
+    public function createPerson()
+    {
+        return view("pages.create-person");
+    }
+
+    // request
+    public function requestPerson(Request $request)
+    {
+        $data = $request->validate(
+            [
+                "firstName" => "string|required|max:32",
+                "lastName" => "string|required|max:32",
+                "dateOfBirth" => "date|required",
+                "height" => "required|integer|min:40|max:260"
+            ]
+
+        );
+        $person = new Person();
+        $person->firstName = $data["firstName"];
+        $person->lastName = $data["lastName"];
+        $person->dateOfBirth = $data["dateOfBirth"];
+        $person->height = $data["height"];
+        $person->save();
+        return redirect()->route("home");
+    }
+
+    // edit
+    public function editPerson(Person $person)
+    {
+        return view("pages.edit-person", compact("person"));
+    }
+
+    // update
+    public function updatePerson(Request $request, Person $person)
+    {
+        $data = $request->validate(
+            [
+                "firstName" => "string|required|max:32",
+                "lastName" => "string|required|max:32",
+                "dateOfBirth" => "date|required",
+                "height" => "required|integer|min:40|max:260"
+            ]
+
+        );
+        $person->firstName = $data["firstName"];
+        $person->lastName = $data["lastName"];
+        $person->dateOfBirth = $data["dateOfBirth"];
+        $person->height = $data["height"];
+        $person->save();
+
+        return redirect()->route("home");
+    }
 }
